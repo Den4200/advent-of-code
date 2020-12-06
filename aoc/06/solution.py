f641581df28bfa5e0a4aa014d70c448614855cda
Den4200 <dennis@dennispham.me>
2020-12-06 00:22:44 -0500

    Add day 6 solution.

---
+++ aoc/06/solution.py
@@ -0,0 +1,24 @@
+import re
+from collections import Counter
+
+
+def parse_data():
+    with open('aoc/06/input.txt') as f:
+        data = f.read()
+
+    return [(re.findall(r'\w', line), len(line.splitlines())) for line in data.split('\n\n')]
+
+
+def part_one(data):
+    return sum(len(set(group[0])) for group in data)
+
+
+def part_two(data):
+    return sum(len([k for k, v in Counter(group[0]).items() if v == group[1]]) for group in data)
+
+
+def main():
+    data = parse_data()
+
+    print(f'Day 06 Part 01: {part_one(data) or "skipped"}')
+    print(f'Day 06 Part 02: {part_two(data) or "skipped"}')
