d0ef25818e21ecb1debf3f541377f638edd92e00
Den4200 <dennis@dennispham.me>
2020-12-08 00:57:03 -0500

    Add day 8 solution.

---
+++ aoc/08/solution.py
@@ -0,0 +1,60 @@
+import re
+
+
+def parse_data():
+    with open('aoc/08/input.txt') as f:
+        data = f.read()
+
+    return [re.fullmatch(r'([a-z]{3}) ((?:\+|-)\d+)', line).groups() for line in data.splitlines()]
+
+
+def execute(data):
+    acc = 0
+    instructions = set()
+
+    idx = 0
+    while idx < len(data):
+        action, value = data[idx]
+
+        if action == 'acc':
+            acc += int(value)
+            idx += 1
+        elif action == 'jmp':
+            idx += int(value)
+        else:
+            idx += 1
+
+        if idx not in instructions:
+            instructions.add(idx)
+        else:
+            break
+
+    return idx, acc
+
+
+def part_one(data):
+    return execute(data)[1]
+
+
+def part_two(data):
+    for i, instruction in enumerate(data):
+        action, value = instruction
+
+        if action == 'jmp':
+            idx, acc = execute(data[:i] + [('nop', int(value))] + data[i + 1:])
+
+        elif action == 'nop':
+            idx, acc = execute(data[:i] + [('jmp', int(value))] + data[i + 1:])
+
+        else:
+            continue
+
+        if idx == len(data):
+            return acc
+
+
+def main():
+    data = parse_data()
+
+    print(f'Day 08 Part 01: {part_one(data) or "skipped"}')
+    print(f'Day 08 Part 02: {part_two(data) or "skipped"}')
