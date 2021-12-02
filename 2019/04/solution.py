062d12e4149388a8bfaf454710e9b32fdf8c5915
Den4200 <dennis@dennispham.me>
2021-12-01 23:05:49 -0500

    Refactor and optimize 2019 day 4 solution.

--- 2019/04/solution.py
+++ 2019/04/solution.py
@@ -8,43 +8,28 @@ def parse_data():
     return [int(num) for num in data.split("-")]
 
 
-def part_one(data):
+def generate_passwords(data, compare_func):
     total = 0
 
     for num in range(data[0], data[1] + 1):
         places = str(num)
 
         for prev, curr in zip(places, places[1:]):
-            if prev == curr:
-                break
-        else:
-            continue
-
-        for prev, curr in zip(places, places[1:]):
             if curr < prev:
                 break
         else:
-            total += 1
+            if any(compare_func(len("".join(g)), 2) for _, g in groupby(places)):
+                total += 1
 
     return total
+
+
+def part_one(data):
+    return generate_passwords(data, int.__ge__)
 
 
 def part_two(data):
-    total = 0
-
-    for num in range(data[0], data[1] + 1):
-        places = str(num)
-
-        if not any(len("".join(g)) == 2 for _, g in groupby(places)):
-            continue
-
-        for prev, curr in zip(places, places[1:]):
-            if curr < prev:
-                break
-        else:
-            total += 1
-
-    return total
+    return generate_passwords(data, int.__eq__)
 
 
 def main():
