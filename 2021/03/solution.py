42ca59621f0c129520133307ee1997fc633b7b0a
Den4200 <dennis@dennispham.me>
2021-12-03 08:49:52 -0500

    Refactor bit shifts for day 3.

--- 2021/03/solution.py
+++ 2021/03/solution.py
@@ -14,22 +14,22 @@ def part_one(data):
     bits = defaultdict(int)
     for binary in data:
         for i in range(12):
-            bits[i] += 1 if binary & (1 << (binary_length - 1) >> i) > 0 else 0
+            bits[i] += 1 if binary & (1 << (binary_length - i - 1)) > 0 else 0
 
     gamma = 0
     for i, amt in bits.items():
         if amt >= len(data) >> 1:
-            gamma |= 1 << (binary_length - 1) >> i
+            gamma |= 1 << (binary_length - i - 1)
 
     return gamma * (gamma ^ 2 ** binary_length - 1)
 
 
 def gas_filter(binary_length, data, compare_func):
     for i in range(12):
-        ones = sum(binary & (1 << (binary_length - 1) >> i) > 0 for binary in data)
+        ones = sum(binary & (1 << (binary_length - i - 1)) > 0 for binary in data)
 
         bit_compare = int.__gt__ if compare_func(ones, len(data) - ones) else int.__eq__
-        data = [binary for binary in data if bit_compare(binary & (1 << (binary_length - 1) >> i), 0)]
+        data = [binary for binary in data if bit_compare(binary & (1 << (binary_length - i - 1)), 0)]
 
         if len(data) == 1:
             break
