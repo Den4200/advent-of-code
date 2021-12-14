4ba1f03b475858ebb0b9bcf2ed7f7a1442deb474
Den4200 <dennis@dennispham.me>
2021-12-14 08:15:08 -0500

    Add day 14 solution.

---
+++ 2021/14/solution.py
@@ -0,0 +1,46 @@
+import re
+from collections import Counter, defaultdict
+
+
+def parse_data():
+    with open('2021/14/input.txt') as f:
+        data = f.read()
+
+    polymer_template = list(data.split(maxsplit=1)[0])
+
+    pair_insertions = dict(re.findall(r"([A-Z]{2}) -> ([A-Z])", data))
+    return polymer_template, pair_insertions
+
+
+def npolymers(polymers, pair_insertions, steps):
+    pair_count = Counter("".join(pair) for pair in zip(polymers, polymers[1:]))
+
+    for _ in range(steps):
+        new_pair_counts = defaultdict(int)
+        for pair in pair_count:
+            new_pair_counts[pair[0] + pair_insertions[pair]] += pair_count[pair]
+            new_pair_counts[pair_insertions[pair] + pair[1]] += pair_count[pair]
+
+        pair_count = new_pair_counts
+
+    polymer_counts = defaultdict(int)
+    for pair in pair_count:
+        polymer_counts[pair[0]] += pair_count[pair]
+
+    polymer_counts[polymers[-1]] += 1
+    return max(polymer_counts.values()) - min(polymer_counts.values())
+
+
+def part_one(data):
+    return npolymers(*data, 10)
+
+
+def part_two(data):
+    return npolymers(*data, 40)
+
+
+def main():
+    data = parse_data()
+
+    print(f'Day 14 Part 01: {part_one(data)}')
+    print(f'Day 14 Part 02: {part_two(data)}')
