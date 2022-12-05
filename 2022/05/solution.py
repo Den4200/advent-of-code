bf00a5d230046dda42eb46347c0b17c13e7e9230
Den4200 <dennis@dennispham.me>
2022-12-05 00:36:37 -0500

    Add day 5 solutions.

---
+++ 2022/05/solution.py
@@ -0,0 +1,46 @@
+from copy import deepcopy
+import re
+
+import numpy as np
+
+
+def parse_data():
+    with open("2022/05/input.txt") as f:
+        data = f.readlines()
+
+    crates = np.transpose([[line[i] for i in range(1, len(line), 4)] for line in data[:8]])[::,::-1]
+    crates = [[crate for crate in stack if crate != " "] for stack in crates]
+    moves = [tuple(map(int, re.findall("\d+", line))) for line in data[10:]]
+
+    return {
+        "crates": crates,
+        "moves": moves,
+    }
+
+
+def part_one(data):
+    crates = deepcopy(data["crates"])
+    moves = data["moves"]
+
+    for move in moves:
+        for _ in range(move[0]):
+            crates[move[2] - 1].append(crates[move[1] - 1].pop())
+
+    return "".join(stack[-1] for stack in crates)
+
+
+def part_two(data):
+    crates = data["crates"]
+    moves = data["moves"]
+
+    for move in moves:
+        crates[move[2] - 1] += reversed([crates[move[1] - 1].pop() for _ in range(move[0])])
+
+    return "".join(stack[-1] for stack in crates)
+
+
+def main():
+    data = parse_data()
+
+    print(f"Day 05 Part 01: {part_one(data)}")
+    print(f"Day 05 Part 02: {part_two(data)}")
