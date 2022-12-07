ea8020e22c6f189e040d2b9f75f143dce698005a
Den4200 <dennis@dennispham.me>
2022-12-07 16:46:05 -0500

    Refactor day 4 solution.

--- 2022/04/solution.py
+++ 2022/04/solution.py
@@ -3,25 +3,20 @@ def parse_data():
         data = f.read()
 
     return [
-        [tuple(map(int, p.split("-"))) for p in line.split(",")]
+        [[int(n) for n in p.split("-")] for p in line.split(",")]
         for line in data.splitlines()
     ]
 
 
 def part_one(data):
     return sum(
-        (p1[0] in range(p2[0], p2[1] + 1) and p1[1] in range(p2[0], p2[1] + 1))
-        or (p2[0] in range(p1[0], p1[1] + 1) and p2[1] in range(p1[0], p1[1] + 1))
+        p1[0] <= p2[0] and p1[1] >= p2[1] or p1[0] >= p2[0] and p1[1] <= p2[1]
         for p1, p2 in data
     )
 
 
 def part_two(data):
-    return sum(
-        (p1[0] in range(p2[0], p2[1] + 1) or p1[1] in range(p2[0], p2[1] + 1))
-        or (p2[0] in range(p1[0], p1[1] + 1) or p2[1] in range(p1[0], p1[1] + 1))
-        for p1, p2 in data
-    )
+    return sum(p1[0] <= p2[1] and p1[1] >= p2[0] for p1, p2 in data)
 
 
 def main():
