5755294301a75524a6d13d20655302eaebbe20b8
Dennis Pham <dennis@dennispham.me>
2023-12-03 04:13:52 -0500

    Attempting to oneline day 3?

--- 2023/03/solution.py
+++ 2023/03/solution.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from functools import partial, reduce
 from math import prod
 
 import numpy as np
@@ -9,8 +10,7 @@ def parse_data():
         return np.pad([[*line.strip()] for line in f], (2, 2), "constant", constant_values=".")
 
 
-def part_one(data):
-    s = 0
+def gear_windows(data):
     for y, line in enumerate(data[2:-2], start=2):
         x0 = -1
 
@@ -20,37 +20,38 @@ def part_one(data):
                     x0 = x
 
             elif x0 != -1:
-                window = data[y - 1:y + 2, x0 - 1:x + 1]
-
-                if np.any((window != ".") & (~np.char.isdigit(window))):
-                    s += int("".join(line[x0:x]))
-
+                yield x0, x, y, data[y - 1:y + 2, x0 - 1:x + 1]
                 x0 = -1
 
-    return s
+
+def part_one(data):
+    return sum(
+        int("".join(data[y][x0:x]))
+        for x0, x, y, window in gear_windows(data)
+        if np.any((window != ".") & (~np.char.isdigit(window)))
+    )
+
+
+def reduce_gears(data, gears, window_data):
+    x0, x, y, window = window_data
+
+    if np.any(window == "*"):
+        dy, dx = np.where(window == "*")
+        gears[y + dy[0] - 1, x0 + dx[0] - 1].append(int("".join(data[y][x0:x])))
+
+    return gears
 
 
 def part_two(data):
-    gears = defaultdict(lambda: [])
-
-    for y, line in enumerate(data[2:-2], start=2):
-        x0 = -1
-
-        for x, c in enumerate(line[2:-1], start=2):
-            if c.isdigit():
-                if x0 == -1:
-                    x0 = x
-
-            elif x0 != -1:
-                window = data[y - 1:y + 2, x0 - 1:x + 1]
-
-                if np.any(window == "*"):
-                    dy, dx = np.where(window == "*")
-                    gears[y + dy[0] - 1, x0 + dx[0] - 1].append(int("".join(line[x0:x])))
-
-                x0 = -1
-
-    return sum(prod(gear) for gear in gears.values() if len(gear) > 1)
+    return sum(
+        prod(gear)
+        for gear in reduce(
+            partial(reduce_gears, data),
+            gear_windows(data),
+            defaultdict(lambda: []),
+        ).values()
+        if len(gear) > 1
+    )
 
 
 def main():
